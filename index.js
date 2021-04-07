a47c2e9a0863ac62ed11b6c798ff6328fee1b91c
Yeasin <yeasin30112001@gmail.com>
2021-04-07 17:08:03 -0600

    delete, post, get method added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
+const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
 const port = 5000;
 
@@ -12,8 +13,45 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const cartFoodsCollection = client.db("hotOnionRestaurantCustomersCartFoods").collection("cartFoods");
+  const orderedFoodsCollection = client.db("hotOnionRestaurantCustomersCartFoods").collection("orderedFoods");
   app.post('/addToCart', (req, res) => {
-      console.log(req.body);
+      const cartFoodInfo = req.body;
+      cartFoodsCollection.insertOne(cartFoodInfo)
+      .then(result => {
+        result.insertedCount > 0 && res.send(!!result.insertedCount);
+      })
+  });
+
+  app.post('/placeOrder', (req, res) => {
+      const orderedData = req.body;
+      orderedFoodsCollection.insertOne(orderedData)
+      .then(result => {
+        result.insertedCount > 0 && res.send(!!result.insertedCount);
+      })
+  });
+
+  app.get('/cartFoods/:email', (req, res) => {
+    const email = req.params.email
+    cartFoodsCollection.find({email: email})
+    .toArray((err, foods) => {
+      res.send(foods)
+    })
+  })
+
+  app.get('/orders/:email', (req, res) => {
+    const email = req.params.email
+    orderedFoodsCollection.find({email: email})
+    .toArray((err, foods) => {
+      res.send(foods)
+    })
+  })
+
+  app.delete('/removeFromCart/:id', (req, res) => {
+    const id = req.params.id
+    cartFoodsCollection.deleteOne({_id: ObjectId(id)})
+    .then(result => {
+      result.deletedCount > 0 && res.send(!!result.deletedCount);
+    })
   })
 });
 
@@ -22,6 +60,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
